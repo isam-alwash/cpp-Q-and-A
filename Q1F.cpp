f5c2dcda00ee58fae7a131343909158e5d6aa635
isam-alwash <janaramyisam@gmail.com>
2024-03-18 10:02:01 +0300

    git -m commit"24/3/18"

---
+++ Q1F.cpp
@@ -0,0 +1,16 @@
+/*
+Write a c++ program to find the power of an integer without using cmath header. 
+*/
+#include <iostream>
+using namespace std;
+double power( int x , int y){
+    int pow = 1;   
+    for(int i = 0; i < y;i++){
+    pow *= x;
+    }
+    return pow ;
+}
+int main(){
+    cout<<power(2,4);
+ return 0;   
+}
